bb54f7250e2cc05c45b49c4258b5d0654f78d183
penguinasian <zhangqiangtianchen@gmail.com>
2023-01-16 10:49:19 -0800

    added StateContext and implemented adding quantity

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,10 +1,15 @@
 import '@/styles/globals.css';
+import { Toaster } from 'react-hot-toast';
 import { Layout } from '@/components';
+import { StateContext } from '@/context/StateContext';
 
 export default function App({ Component, pageProps }) {
   return (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   )
 }
